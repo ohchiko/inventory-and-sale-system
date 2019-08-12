f69da0771d0a5d938af743532dbcdf7fcb8d83d9
Gregorio Chiko Putra <chiko150@outlook.com>
2019-08-12 11:55:52 +0700

    configured api authorization using laravel's api_token

--- app/User.php
+++ app/User.php
@@ -6,10 +6,12 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasRoles;
+    use Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -37,4 +39,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function boot() {
+        parent::boot();
+
+        static::created(function ($user) {
+            $user->api_token = Str::random(60);
+
+            $user->save();
+        });
+    }
 }
